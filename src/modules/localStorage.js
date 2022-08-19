d6d934ce9cd7095a5695f92f68368528ed6bbed1
Bantealem <bantem2021@gmail.com>
2022-08-19 17:11:43 +0300

    fix linters

--- src/modules/localStorage.js
+++ src/modules/localStorage.js
@@ -2,6 +2,7 @@ class LocalStorage {
     static set = (data) => {
       localStorage.setItem('TODO-LIST', JSON.stringify(data));
     };
-     static get = () => JSON.parse(localStorage.getItem('TODO-LIST')) || [];
+    
+    static get = () => JSON.parse(localStorage.getItem('TODO-LIST')) || [];
 }
 export default LocalStorage;

88de9840085be0694482bd31f460de3b690861d7
Bantealem <bantem2021@gmail.com>
2022-08-20 20:47:19 +0300

    add and remove

--- src/modules/task.js
+++
@@ -1,9 +0,0 @@
-import { todoTasks } from './variable.js';
-
-export default class Task {
-  constructor(userTask) {
-    this.id = todoTasks.length + 1;
-    this.description = userTask.value;
-    this.completed = false;
-  }
-}
--- src/modules/variable.js
+++ src/modules/variable.js
@@ -1,6 +1,6 @@
 import LocalStorage from './localStorage.js';
 
 export const todoTasks = LocalStorage.get() || [];
-export const todoContainer = document.querySelector('.todo-box');
+export const todoContainer = document.getElementById('todo-box');
 export const clearAllCompletedBtn = document.querySelector('.clear-btn');
 export const userTask = document.querySelector('.add-input');
